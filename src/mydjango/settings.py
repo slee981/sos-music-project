324cbf9a8e4804dcddecc15154bdee7bb057a8d0
Felipe de Oliveira <felipedeoliveira@Raymonds-MBP-6.lan>
2020-04-06 23:11:35 -0500

    Database issues solved

--- src/mydjango/settings.py
+++ src/mydjango/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sos_db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
